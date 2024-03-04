41da0e7bf57a59caa13f259a6da209d81eac8b89
Andreia Dourado <deiadourado93@gmail.com>
2024-03-04 09:23:30 -0500

    updated database controller

--- src/main/java/org/example/f1app/DatabaseController.java
+++ src/main/java/org/example/f1app/DatabaseController.java
@@ -6,20 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseController {
     // Database connection details
-    private static final String URL = "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5686959"; // Database URL
-    private static final String USERNAME = "sql5686959"; // Database username
-    private static final String PASSWORD = "9Ktb5x1RiC"; // Database password
-
-    static {
-        try {
-            // Load the MySQL JDBC driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            // Print error message if the driver cannot be loaded
-            System.err.println("Error loading MySQL JDBC driver: " + e.getMessage());
-            e.printStackTrace(); // Print the stack trace for debugging
-        }
-    }
+    private static final String URL = "jdbc:sqlserver://deiadourado.database.windows.net:1433;database=FormulaOne;"; // Database URL
+    private static final String USERNAME = "deia@deiadourado"; // Database username
+    private static final String PASSWORD = "Formulaone1"; // Database password
 
     // Method to establish a database connection
     public static Connection getConnection() throws SQLException {
